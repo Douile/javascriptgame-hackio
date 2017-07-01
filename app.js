4ece20c56fc7e5635a3d7e0cdf6205f12e72a4c6
Tom <TheOnly-Tom@users.noreply.github.com>
2017-07-01 23:15:33 +0100

    Updated Comments
    Updated comments
    Removed barely working command history functionality

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ author: Tom Hipwell https://github.com/TheOnly-Tom/
 */
 
 // Functions
-function getTime() {
+function getTime() { // returns time for log
   time = new Date();
   timer = time.getDate() + "/" + time.getMonth() + "/" + time.getFullYear() + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + ":" + time.getMilliseconds();
   return timer
 }
-function getCodeTime() {
+function getCodeTime() { // returns a single integer time for use in code
   time = new Date();
   return time.getTime();
 }
-function mobile() {
+function mobile() { // Checks whether user is on mobile copied from http://detectmobilebrowsers.com
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   app.vars.enviroment.mobile = check;
@@ -27,7 +27,7 @@ function logItem(type,text) { // object for every log item
   this.text = text;
   return this;
 }
-var protectedStorage = function() {
+var protectedStorage = function() { // storage that limits modification
   var text = "";
   function changeTo(val) {
     text = text + val[0];
@@ -42,7 +42,7 @@ var protectedStorage = function() {
   }
 }
 // Prototypes
-String.prototype.count = function(char) {
+String.prototype.count = function(char) { // counts how many times a character is found in a string
   count = 0;
   for (i=0;i<this.length;i++) {
     if (this[i] == char) {
@@ -51,13 +51,13 @@ String.prototype.count = function(char) {
   }
   return count;
 }
-String.prototype.download = function(name) {
+String.prototype.download = function(name) { // downloads a string
   a = document.createElement("a");
   a.href = "data:text/plain,"+encodeURIComponent(this.valueOf());
   a.download = name;
   a.click();
 }
-String.prototype.backspace = function() {
+String.prototype.backspace = function() { // returns the string with the last character removed
   temp = this.valueOf();
   text = "";
   for (i=0;i<temp.length-1;i++) {
@@ -584,7 +584,6 @@ var app = {
         left: 5
       }
       this.failedCommands = 0;
-      this.commandLog = [];
       this.vars = {};
       this.varsetup = app.vars.levels[this.level]["vars"]["setup"];
       this.varsetup();
@@ -637,15 +636,7 @@ var app = {
             app.runtime.objects[app.scenes.current_cmd].text = app.runtime.objects[app.scenes.current_cmd].text.backspace();
           } else if (e.keyCode == 13) {
             app.runtime.objects[app.scenes.current_cmd].commandHandler();
-          } else if (e.keyCode == 38) {
-            if (app.runtime.objects[app.scenes.current_cmd].commandLog["current"] > app.runtime.objects[app.scenes.current_cmd].commandLog.length && app.runtime.objects[app.scenes.current_cmd].text != "") {
-              app.runtime.objects[app.scenes.current_cmd].commandLog.push(app.runtime.objects[app.scenes.current_cmd].text);
-            }
-            if (app.runtime.objects[app.scenes.current_cmd].commandLog["current"] > 1) {
-              app.runtime.objects[app.scenes.current_cmd].commandLog["current"] -= 1;
-              app.runtime.objects[app.scenes.current_cmd].text = app.runtime.objects[app.scenes.current_cmd].commandLog[app.runtime.objects[app.scenes.current_cmd].commandLog["current"]];
-            }
-          }
+          } 
         }
       }
       this.resizeHandler = function(e) {
@@ -665,11 +656,6 @@ var app = {
           location = "https://github.com/TheOnly-Tom/javascriptgame-hackio";
         } else {
           this.commandHandlerEx(command,args);
-        }
-        if (this.text != "") {
-          this.commandLog.push(this.text);
-          this.commandLog["current"] = this.commandLog.length;
-          this.text = "";
         }
       }
       this.commandHandlerEx = app.vars.levels[this.level]["cmds"];
@@ -740,6 +726,7 @@ var app = {
 
 // main code
 mobile();
+
 app.init(); // calls app init to prepare app
 
 app.runtime.start();
